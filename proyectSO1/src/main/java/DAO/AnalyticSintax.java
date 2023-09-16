80126ec19bc704af0cb6b2aeb9a9cca4584dbce0
CAAC89 <Cailis2010@hotmail.com>
2023-09-16 14:18:07 -0600

    Error

---
+++ proyectSO1/src/main/java/DAO/AnalyticSintax.java
@@ -0,0 +1,20 @@
+package DAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Caili
+ */
+public class AnalyticSintax {
+    public List<Error> getErrors(String location){
+        List<Error> errors = new ArrayList<>();
+        return errors;
+    }
+}

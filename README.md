4f28d3af97c74f3d4e06062a411f0a7366d81387
Alex Sánchez C <asmalexander@hotmail.com>
2023-09-21 22:22:07 -0600

    Video

--- README.md
+++ README.md
@@ -5,10 +5,12 @@
 ### 2018014778 Alexander Sanchez Cespedez
 
 ### Estado del proyecto: Escala de 1 a 5, 1 siendo completo y 5 deplorable
-### Enlace del video: (Si esto fue solicitado)
+### Enlace del video: https://youtu.be/S_q7QkexvYg
 Recordar que el video debe ser público y con sonido para ser visto por el profesor
 
 Faltantes
 Ejecucion Automatica
 Estadisticas
 Cambios de estado
+Validaciones de casos muy especificos
+Bugs en ejecucion de algunas instrucciones
--- proyectSO1/src/main/java/com/mycompany/proyectso1/ProyectSO1.java
+++ proyectSO1/src/main/java/com/mycompany/proyectso1/ProyectSO1.java
@@ -4,17 +4,7 @@
 
 package com.mycompany.proyectso1;
 
-import DAO.AnalyticSintax;
-import DAO.Methods;
-import DTO.Document;
-import DTO.ErrorFail;
-import DTO.Memory;
-import DTO.Registers;
 import GUI.Principal;
-import GUI.Settings;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
 
 /**
  *
@@ -32,22 +22,5 @@ public class ProyectSO1 {
         Principal pr = new Principal();
         pr.show();
         
-        
-        
-        
-        
-        
-        
-        
-        //routes.add("T:\\2023\\S2\\SO\\PRY\\P1\\proyecto-1-p1-carlos-alex\\proyectSO1\\src\\main\\java\\Examples\\example1.asm"); //cambie las loc y prueba, se imprime la memoria
-        //routes.add("T:\\2023\\S2\\SO\\PRY\\P1\\proyecto-1-p1-carlos-alex\\proyectSO1\\src\\main\\java\\Examples\\example2.asm");
-        //routes.add("T:\\2023\\S2\\SO\\PRY\\P1\\proyecto-1-p1-carlos-alex\\proyectSO1\\src\\main\\java\\Examples\\example3.asm");
-        //routes.add("T:\\2023\\S2\\SO\\PRY\\P1\\proyecto-1-p1-carlos-alex\\proyectSO1\\src\\main\\java\\Examples\\example4.asm");
-        
-        //m.loadFile(routes, document);
-        //System.out.println(document);
-        //m.loadMemoryReserved(64, 512, document); //carga los programas 
-        
-        //System.out.println(document);
     }
 }
